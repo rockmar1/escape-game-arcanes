7014527ab2230667b26acfd403912a45271e119d
Rockmar <33753269+rockmar1@users.noreply.github.com>
2025-09-01 16:00:03 +0200

    Update debug.js

--- js/debug.js
+++ js/debug.js
@@ -1,3 +1,11 @@
-export function dlog(msg) { console.log("[DBG]", msg); }
-export function dwarn(msg) { console.warn("[WARN]", msg); }
-export function derr(msg) { console.error("[ERR]", msg); }
+export function dlog(msg) {
+  console.log(`[DBG] ${msg}`);
+}
+
+export function dwarn(msg) {
+  console.warn(`[WARN] ${msg}`);
+}
+
+export function derr(msg) {
+  console.error(`[ERR] ${msg}`);
+}
