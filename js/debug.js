6afd9ed43c075d386da07be7da879d3e46f2f453
Rockmar <33753269+rockmar1@users.noreply.github.com>
2025-09-01 15:39:30 +0200

    Update debug.js

--- js/debug.js
+++ js/debug.js
@@ -1,18 +1,3 @@
-// debug.js
-export function dlog(msg) {
-    console.log("[DBG]", msg);
-    const log = document.getElementById("debug-log");
-    if(log) { log.textContent += "[DBG] " + msg + "\n"; log.scrollTop = log.scrollHeight; }
-}
-
-export function derr(msg, err) {
-    console.error("[ERR]", msg, err || "");
-    const log = document.getElementById("debug-log");
-    if(log) { log.textContent += "[ERR] " + msg + " " + (err || "") + "\n"; log.scrollTop = log.scrollHeight; }
-}
-
-export function dwarn(msg) {
-    console.warn("[WARN]", msg);
-    const log = document.getElementById("debug-log");
-    if(log) { log.textContent += "[WARN] " + msg + "\n"; log.scrollTop = log.scrollHeight; }
-}
+export function dlog(msg) { console.log("[DBG]", msg); }
+export function dwarn(msg) { console.warn("[WARN]", msg); }
+export function derr(msg) { console.error("[ERR]", msg); }
