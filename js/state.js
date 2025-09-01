fe21bc9546f3ae9864caca9c7e0601fcae5881d0
Rockmar <33753269+rockmar1@users.noreply.github.com>
2025-09-01 16:00:59 +0200

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,22 +1,25 @@
+import { dlog } from "./debug.js";
+
 let state = {
   playerName: "",
   score: 0,
   debug: true
 };
 
+// Joueur
 export function setPlayerName(name) {
   state.playerName = name;
-  const hud = document.getElementById("hud-player");
-  if(hud) hud.textContent = `👤 ${name}`;
+  const el = document.getElementById("hud-player");
+  if (el) el.textContent = `👤 ${name}`;
+  dlog(`Pseudo défini : ${name}`);
 }
 export function getPlayerName() { return state.playerName; }
 
+// Score
 export function setScore(value) {
   state.score = value;
   const el = document.getElementById("score");
-  if(el) el.textContent = `⭐ ${value}`;
+  if (el) el.textContent = `⭐ ${value}`;
 }
 export function addScore(amount) { setScore(state.score + amount); }
 export function getScore() { return state.score; }
-
-export function toggleDebug(on) { state.debug = on; }
