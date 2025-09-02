995cf191a593330f8666613e2eb42681f211f7d1
Rockmar <33753269+rockmar1@users.noreply.github.com>
2025-09-02 17:31:34 +0200

    Update plume.js

--- js/plume.js
+++ js/plume.js
@@ -1,33 +1,14 @@
-// plume.js : Effets d'écriture type plume dans un grimoire
-export function typeWriterEffect(element, text, speed = 50) {
-  element.textContent = "";
+export function typeWriterEffect(element, text, speed = 50, callback) {
   let i = 0;
-  const interval = setInterval(() => {
+  element.textContent = "";
+  function typing() {
     if (i < text.length) {
       element.textContent += text.charAt(i);
       i++;
-    } else {
-      clearInterval(interval);
+      setTimeout(typing, speed);
+    } else if (callback) {
+      callback();
     }
-  }, speed);
+  }
+  typing();
 }
-
-// Effets d'intro multiples
-export const intros = [
-  "Bienvenue jeune aventurier, le royaume vous attend...",
-  "La nuit tombe sur le royaume oublié, votre destin commence ici...",
-  "Les étoiles s'alignent pour révéler votre chemin..."
-];
-
-// Effets de fin personnalisées
-export const victoryMessages = [
-  "Félicitations, héros ! Le royaume est sauvé !",
-  "Votre courage et votre intelligence ont triomphé !",
-  "Les arcanes du royaume vous remercient !"
-];
-
-export const defeatMessages = [
-  "Le royaume s'effondre... Votre mission échouée.",
-  "Les ténèbres ont vaincu, le royaume est perdu.",
-  "Vous avez échoué, mais l'aventure continue..."
-];
